3c0f917358f064d30a3013d13f949a71cbf73f8c
Victor Gasior <vicagasi@ut.utm.edu>
2022-01-18 20:21:04 -0600

    Add project files.

---
+++ Hello World PA1/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World_PA1
+{
+    class Animal
+    {
+        private string name, sound;
+        private int age;
+        private double weight;
+
+        public Animal()
+        {
+            name = "Null";
+            sound = "bzzzzz";
+            age = 1;
+            weight = 1.0;
+        }
+
+        public void printAnimal()
+        {
+
+        }
+
+        public void makeNoise()
+        {
+            Console.WriteLine(name + " says " + sound);
+        }
+
+        public void ageUp()
+        {
+
+        }
+    }
+}
---
+++ Hello World PA1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World_PA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            Console.ReadKey();
+        }
+    }
+}
