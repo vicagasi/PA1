afbda733bb20839b50dc7f2ad42df67a8c8a1b89
Victor Gasior <vicagasi@ut.utm.edu>
2022-01-18 18:24:37 -0600

    * Figured out how to do abstract classes? + Made concrete Cat class

--- Hello World PA1/Animal.cs
+++ Hello World PA1/Animal.cs
@@ -10,8 +10,9 @@ using System.Text;
 
 namespace Hello_World_PA1
 {
-    class Animal
+    abstract class Animal
     {
+        // Properties
         private string name, sound;
         private int age;
         private double weight;
@@ -37,33 +38,75 @@ namespace Hello_World_PA1
 
         //This functions prints details about the animal
         // Result: Info about the animal is printed to the console
-        public void printAnimal() 
+        abstract public void PrintAnimal();
+
+        //This functions has the animal make a noise
+        // Result: Animals noise is printed to the console
+        abstract public void MakeNoise();
+
+        //This function increases the animals age
+        // Result: Animals age is increased by one year, with weight increase varying by animal children
+        abstract public void AgeUp();
+    }
+
+    class Cat : Animal
+    {
+        // Properties
+        private string name, sound, breed;
+        private int age;
+        private double weight;
+
+        // Default constructor for Cat
+        public Cat()
+        {
+            name = "Garfield";
+            sound = "meow";
+            age = 1;
+            weight = 1.0;
+            breed = "Tabby";
+        }
+
+        // Parameterized constructor for Cat
+        // Inputs: string name, sound and breed, int age, double weight
+        public Cat(string name, string sound, string breed, int age, double weight)
+        {
+            this.name = name;
+            this.sound = sound;
+            this.age = age;
+            this.weight = weight;
+            this.breed = breed;
+        }
+
+        //This functions prints details about the animal
+        // Result: Info about the animal is printed to the console
+        public override void PrintAnimal()
         {
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Age: " + age + " years");
             Console.WriteLine("Weight: " + weight + "lbs.");
             Console.WriteLine("Sound: " + sound);
-            Console.WriteLine(" ");
-            Console.WriteLine("This creature is a strange mass of static, pixels, and burnt fur, the raw primordial ooze that makes all virtual animals.");
+            Console.WriteLine("Breed: " + breed);
         }
 
         //This functions has the animal make a noise
         // Result: Animals noise is printed to the console
-        public void makeNoise() 
+        public override void MakeNoise()
         {
-            Console.WriteLine(name + " says " + sound);
+            Console.WriteLine(name + " says " + "\"" + sound + "\"");
         }
 
         //This function increases the animals age
         // Result: Animals age is increased by one year, with weight increase varying by animal children
-        public void ageUp() 
+        public override void AgeUp()
         {
             age++; // Increase age by one year
 
-            if (age > 10 && weight > 5.0) // Once past a certain age animals grow old and feeble instead of stronger
+            if (age > 12 && weight > 7.0) // Once past a certain age animals grow old and feeble instead of stronger
                 weight = weight - 0.5;
+            else if (weight > 10)
+                weight = weight + 0.5; // Fat cat weight gain
             else
-                weight++; // Normal age weight gain
+                weight++; // Normal cat weight gain
 
             // Console writing
             Console.WriteLine("It's " + name + "'s birthday!");
