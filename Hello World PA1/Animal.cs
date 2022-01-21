a1e79616eaaa6ffd3ec993496772eee983259e2d
Victor Gasior <vicagasi@ut.utm.edu>
2022-01-20 20:20:32 -0600

    * Made improvements? + Added more menu functionality

--- Hello World PA1/Animal.cs
+++ Hello World PA1/Animal.cs
@@ -38,37 +38,59 @@ namespace Hello_World_PA1
 
         //This functions prints details about the animal
         // Result: Info about the animal is printed to the console
-        abstract public void PrintAnimal();
+        public void PrintAnimal()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age + " years");
+            Console.WriteLine("Weight: " + weight + "lbs.");
+            Console.WriteLine("Sound: " + sound);
+        }
 
         //This functions has the animal make a noise
         // Result: Animals noise is printed to the console
-        abstract public void MakeNoise();
+       public void MakeNoise()
+        {
+            Console.WriteLine(name + " says " + "\"" + sound + "\"");
+        }
 
         //This function increases the animals age
         // Result: Animals age is increased by one year, with weight increase varying by animal children
-        abstract public void AgeUp();
+        public void AgeUp()
+        {
+            age++; // Increase age by one year
+
+            if (age > 20 && weight > 10.0) // Once past a certain age animals grow old and feeble instead of stronger
+                weight = weight - 1;
+            else if (weight > 20)
+                weight = weight + 0.8; // Fat animal weight gain
+            else
+                weight++; // Normal animal weight gain
+
+            // Console writing
+            Console.WriteLine("It's " + name + "'s birthday!");
+            Console.WriteLine(name + " is now " + age + " years old, and they weigh " + weight + "lbs. ");
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
     }
 
     class Cat : Animal
     {
         // Properties
-        private string name, sound, breed;
-        private int age;
-        private double weight;
+        private string breed;
 
         // Default constructor for Cat
-        public Cat()
+        public Cat() : base()
         {
-            name = "Garfield";
-            sound = "meow";
-            age = 1;
-            weight = 1.0;
             breed = "Tabby";
         }
 
         // Parameterized constructor for Cat
         // Inputs: string name, sound and breed, int age, double weight
-        public Cat(string name, string sound, string breed, int age, double weight)
+        public Cat(string name, string sound, string breed, int age, double weight) : base(name, sound, age, weight)
         {
             this.name = name;
             this.sound = sound;
@@ -79,20 +101,13 @@ namespace Hello_World_PA1
 
         //This functions prints details about the animal
         // Result: Info about the animal is printed to the console
-        public override void PrintAnimal()
+        public void PrintAnimal()
         {
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Age: " + age + " years");
             Console.WriteLine("Weight: " + weight + "lbs.");
             Console.WriteLine("Sound: " + sound);
             Console.WriteLine("Breed: " + breed);
-        }
-
-        //This functions has the animal make a noise
-        // Result: Animals noise is printed to the console
-        public override void MakeNoise()
-        {
-            Console.WriteLine(name + " says " + "\"" + sound + "\"");
         }
 
         //This function increases the animals age
--- Hello World PA1/Program.cs
+++ Hello World PA1/Program.cs
@@ -5,6 +5,7 @@
 */
 
 using System;
+using System.Collections.Generic;
 
 namespace Hello_World_PA1
 {
@@ -12,16 +13,35 @@ namespace Hello_World_PA1
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.World();
+        }
+
+        // Main driver for the program, allows the user to interact with a console menu for creating and interacting with creatures
+        // Result: User runs through menu, until they exit
+        void World()
+        {
+            // List for all the animals
+            List<Animal> animals = new List<Animal>();
+
             string userSelect = "null"; // Variable for users switch selection
 
             while (true)
             {
                 Console.WriteLine("Please Select an Action: ");
-                Console.WriteLine("(Q) Quit Program ");
+                Console.WriteLine("(0) List Animals ");
+                Console.WriteLine("(1) Create Animal ");
+                Console.WriteLine("(2) Display Animal Info ");
+                Console.WriteLine("(3) Make Animal Noise ");
+                Console.WriteLine("(4) Create Animal ");
+                Console.WriteLine("(5) Age Animal Up ");
+                Console.WriteLine("(Q) Quit ");
                 while (true)
                 {
                     userSelect = Console.ReadLine();
-                    if (userSelect != "q")
+
+                    // Makes sure user makes a valid input
+                    if (userSelect != "q" && userSelect != "Q" && userSelect != "0" && userSelect != "1" && userSelect != "2" && userSelect != "3" && userSelect != "4" && userSelect != "5")
                     {
                         Console.WriteLine("Inccorrect Input, please try again.");
                     }
@@ -31,6 +51,58 @@ namespace Hello_World_PA1
                     }
                 }
 
+                // Different Selection Options
+                if (userSelect == "q" || userSelect == "Q") // Program exit
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+                else if (userSelect == "0") // List Animals
+                {
+                    foreach (Animal aAnimal in animals)
+                    {
+                        
+                        Console.WriteLine(Animal.name);
+                    }
+                }
+                else if (userSelect == "1") // Create Animal
+                {
+                    // Animal Values
+                    string animalName = "Greg";
+                    string animalSound = "meow";
+                    int animalAge = 1;
+                    double animalWeight = 3.0;
+
+                    // Animal Selection
+                    Console.WriteLine("Please Select an Animal: ");
+                    Console.WriteLine("(1) Cat ");
+                    Console.WriteLine("(2) Cassowary");
+                    Console.WriteLine("(3) Capybara");
+
+                    while (true)
+                    {
+                        userSelect = Console.ReadLine();
+
+                        // Makes sure user makes a valid input
+                        if (userSelect != "1" && userSelect != "2" && userSelect != "3")
+                        {
+                            Console.WriteLine("Inccorrect Input, please try again.");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // Actually Creating the Animals
+                    if (userSelect == "1")
+                    {
+                        string catBreed = "Tabby"; // Select cat breed
+
+                        animals.Add(new Cat(animalName, animalSound, catBreed, animalAge, animalWeight));
+                    }
+                        
+                }
 
             }
         }
